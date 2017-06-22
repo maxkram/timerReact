4f129f7872cd74aaad19086b9e1d3324f64fdd26
maxkram <max.kramer@live.com>
2017-06-22 14:59:34 +0300

    inicommit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            Reactивный таймер
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Таймер</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Отсчет</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Сделано <a href="https://github.com/maxkram" target="_blank">maxkram</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Navigation;
