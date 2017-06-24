e8b2a55a7e6097b0c7c3a29a05de971f150a73db
maxkram <max.kramer@live.com>
2017-06-24 14:55:12 +0300

    'countdown functions added'

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,18 +5,17 @@ var CountdownForm = React.createClass({
         e.preventDefault();
         var strSeconds = this.refs.seconds.value;
 
-        if (strSeconds.match(/^[0-9]*$/)){
+        if (strSeconds.match(/^[0-9]*$/)) {
             this.refs.seconds.value = '';
-            this.props.onSetCountdown(parseInt(strSeconds,10));
+            this.props.onSetCountdown(parseInt(strSeconds, 10));
         }
     },
-
     render: function () {
         return (
             <div>
                 <form ref="form" onSubmit={this.onSubmit} className="countdown-form">
-                    <input type="text" ref="seconds" placeholder="Укажите время в секундах"/>
-                    <button className="button expanded">Старт</button>
+                    <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+                    <button className="button expanded">Start</button>
                 </form>
             </div>
         );
